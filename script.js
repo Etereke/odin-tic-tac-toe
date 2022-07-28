2f0f45a97453879be24425530bc08ab98b5e62f6
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-07-28 14:15:40 +0200

    Implement drawing X and O on click

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-const LogicalBoard = (() => {
-    const gameboard = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
-    const getCell = (index) => {
-        return gameboard[index];
-    }
-    return {
-        getCell
-    };
-})();
-
-const PhysicalBoard = (() => {
+const GameBoard = (() => {
     const gameboard = [];
+    let players = [];
     for(let i = 1; i <= 9; i++){
         gameboard.push(document.querySelector(`.cell-${i}`));
     }
 
+    const SetPlayers = (playerList) => players = playerList;
+    const handleClick = (e) => {
+        const currentCellIndex = e.target.dataset.index;
+        GameLogic.AddMark(currentCellIndex);
+        GameBoard.Render();
+    }
+    const InitializeEventHandlers = () => {
+        gameboard.forEach(cell => {
+            cell.addEventListener('click', handleClick);
+        });
+    }
+
     const Render = () => {
-        console.log('Rendering...');
         for(let i = 0; i < 9; i++){
             let displayCell = gameboard[i];
-            let logicCell = LogicalBoard.getCell(i);
+            let logicCell = GameLogic.GetCell(i);
             displayCell.textContent = logicCell;
+            displayCell.style.color = logicCell === players[0].getMarker() ? players[0].getColor() : players[1].getColor();
             if(logicCell === 'X'){
                 displayCell.style.color = 'blue';
             }
@@ -29,27 +32,59 @@ const PhysicalBoard = (() => {
         }
     };
     return {
-        Render
+        Render,
+        InitializeEventHandlers,
+        SetPlayers
     };
 })();
-
-PhysicalBoard.Render();
 const GameLogic = (() => {
+    let firstIsCurrentPlayer = true;
+    let  players = [];
+    let gameboard = ['', '', '', '', '', '', '', '', ''];
 
+    const SetPlayers = (player1, player2) => {
+        players = [player1, player2];
+    }
+    const SendPlayers = () => GameBoard.SetPlayers(players);
+    const SwitchPlayer = () => firstIsCurrentPlayer = !firstIsCurrentPlayer;
+    const GetCell = (index) => {
+        return gameboard[index];
+    }
+
+    const AddMark = (index) => {
+        if(gameboard[index] === ''){
+            gameboard[index] = firstIsCurrentPlayer ? players[0].getMarker() : players[1].getMarker();
+            SwitchPlayer();
+        }
+    }
+
+    const StartGame = (first, second) => {
+        gameboard = ['', '', '', '', '', '', '', '', ''];
+        SetPlayers(first, second);
+        SendPlayers();
+        firstIsCurrentPlayer = true;
+        GameBoard.InitializeEventHandlers();
+        GameBoard.Render();
+    }
     return{
-
+        StartGame,
+        AddMark, 
+        GetCell
     };
 })();
 
-const Player = (name, marker) => {
+const Player = (name, marker, color) => {
     const getName = () => name;
     const getMarker = () => marker;
-
+    const getColor = () => color;
     return {
         getName,
-        getMarker
+        getMarker,
+        getColor
     };
 };
 
-const playerOne = Player("Etereke", 'X');
-const playerTwo = Player("Süti", 'O');
+const playerOne = Player("Etereke", 'X', 'blue');
+const playerTwo = Player("Süti", 'O', 'red');
+
+GameLogic.StartGame(playerOne, playerTwo);
