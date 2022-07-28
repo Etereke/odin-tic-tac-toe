15ee61a81da0ba8a74cd87768e69bfed56201083
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-07-28 12:35:42 +0200

    Implement display of game board

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const Gameboard = (() => {
-    const gameboard = [['X', 'X', 'X'], ['O', 'O', 'O'], ['X', 'X', 'X']];
-    const Render = () => {
-        console.log(gameboard);
+const LogicalBoard = (() => {
+    const gameboard = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
+    const getCell = (index) => {
+        return gameboard[index];
     }
+    return {
+        getCell
+    };
+})();
+
+const PhysicalBoard = (() => {
+    const gameboard = [];
+    for(let i = 1; i <= 9; i++){
+        gameboard.push(document.querySelector(`.cell-${i}`));
+    }
+
+    const Render = () => {
+        console.log('Rendering...');
+        for(let i = 0; i < 9; i++){
+            let displayCell = gameboard[i];
+            let logicCell = LogicalBoard.getCell(i);
+            displayCell.textContent = logicCell;
+            if(logicCell === 'X'){
+                displayCell.style.color = 'blue';
+            }
+            else{
+                displayCell.style.color = 'red';
+            }
+        }
+    };
     return {
         Render
     };
 })();
 
+PhysicalBoard.Render();
 const GameLogic = (() => {
 
     return{
